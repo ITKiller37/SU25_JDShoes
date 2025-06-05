6a65985090c5f289b4f7c286b5d92dbca28c4862
ITKiller37 <haidangpham2001@gmail.com>
2025-06-05 17:04:01 +0700

    init commit

---
+++ src/test/java/com/example/jdshoes/JdShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
