7821f155cf9e09d77e66223c4d43bc35486cf18b
ITKiller37 <haidangpham2001@gmail.com>
2025-07-13 23:32:53 +0700

    Commit 13/72

--- src/main/java/com/example/jdshoes/service/ColorService.java
+++ src/main/java/com/example/jdshoes/service/ColorService.java
@@ -39,40 +39,4 @@ public interface ColorService {
 
     List<Color> getColorsByProductId(Long productId);
 }
-=======
-package com.example.jdshoes.service;
-
-import com.example.jdshoes.dto.Color.ColorDto;
-import com.example.jdshoes.entity.Color;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public interface ColorService {
-
-    List<Color> getColorByProductId(Long productId);
-
-    List<Color> getColorByProductIdAndSizeId(Long productId, Long sizeId);
-
-    ColorDto createColorApi(ColorDto colorDto);
-
-    Page<Color> findAll(Pageable pageable);
-
-    Color createColor(Color color);
-
-    boolean existsById(Long id);
-
-    Color updateColor(Color color);
-
-    Optional<Color> findById(Long id);
-
-
-    void delete(Long id);
-
-    List<Color> findAll();
-}
 
